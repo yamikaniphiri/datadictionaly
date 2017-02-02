7e1df4ce44171a30564b33818729a666a4bd32e0
yami <yamikanidj@gmial.com>
2017-02-02 22:33:02 +0200

    some comments

--- scripts/services.js
+++ scripts/services.js
@@ -5,6 +5,38 @@
 
 'use strict';
 
+/**
+ * Function will retrieve the root URL for domain it's running on.
+ *
+ * @returns {string} : the currently root hostname
+ */
+function getHostRoot() {
+
+    //return location.protocol + '//' + location.hostname;
+    return location.protocol + '//' + location.hostname + ':' + location.port;
+}
+
+/**
+ * Function will return the subfolder name for the DHIS installation on the server
+ * which is varies from DHIS installation to installation.
+ */
+function getDHISInstallFolder() {
+    var pathArray = window.location.pathname.split('/');
+    //console.log(pathArray[1]);
+    var pathName = pathArray[1];
+    //console.log(pathName);
+    if (pathName == 'apps') {
+        pathName = '';
+    }
+    return pathName;
+}
+
+function getApiRoot() {
+    console.log(getHostRoot() + '/' + getDHISInstallFolder());
+    return getHostRoot() + '/' + getDHISInstallFolder();
+}
+
+
 /* Services */
 
 var appSeedServices = angular.module('appSeedServices', ['ngResource']);
@@ -14,7 +46,7 @@ appSeedServices.factory('dataElement', ['$http', '$q', function ($http, $q) {
             //var url ='https://dhis.cc.ac.mw/dhis/api/dataElements.json?fields=name,id';
 
             listAllMetadata: function (apiResource) {
-                var url = 'https://dhis.cc.ac.mw/dhis/api/'+apiResource+'.json?fields=name,id&paging=false' ;
+                var url = getApiRoot()+'/api/'+apiResource+'.json?fields=name,id&paging=false' ;
                 console.log("proflie metadata "+url);
                 
                 //promise to get all the metadata??
@@ -29,7 +61,7 @@ appSeedServices.factory('dataElement', ['$http', '$q', function ($http, $q) {
             }
             ,
             specificMetadata: function (apiResource, id) {
-                var url = 'https://dhis.cc.ac.mw/dhis/api/'+apiResource+'/'+id+'.json?fields=:allpaging=false' ;
+                var url = getApiRoot()+'/api/'+apiResource+'/'+id+'.json?fields=:allpaging=false' ;
                 console.log("proflie metadata "+url);
                 var def = $q.defer();
                 $http.get(url)
